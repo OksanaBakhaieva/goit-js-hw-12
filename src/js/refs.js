d5b0a09042807ecb9eca582365d4b6346a23c98f
OksanaBakhaieva <obahaeva@gmail.com>
2024-02-02 15:49:02 +0000

    Changes with load-btn, loader

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,9 +2,6 @@ export const refs = {
   form: document.querySelector('.form'),
   input: document.querySelector('.input'),
   gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
-  searchBtn: document.querySelector('.btn'),
-  btnMore: document.querySelector('.load-btn'),
-  // loadMoreBtn: document.querySelector('[data-action="load-more"]'),
-  preloader: document.getElementById("preloader"),
+  button: document.querySelector('.button'),
+  loader: document.querySelector(".loader"),
 };
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { refs } from './js/refs';
 import { getImages } from './js/pixayApi';
 import { createMarkup } from './js/render-function';
 import { scrollBy } from './js/scrollBy';
-import buttonService from './js/buttonService';
+
 
 const simplyGallery = new SimpleLightbox('.gallery-item a', {
         captionsData: 'alt',
@@ -21,21 +21,22 @@ const queryParams = {
 };
 
 refs.form.addEventListener('submit', handleSearch);
+
 async function handleSearch(event) {
   event.preventDefault();
   refs.gallery.innerHTML = '';
-
   queryParams.page = 1;
-  queryParams.query = refs.input.value.trim();
-  
+
+  const form = event.currentTarget;
+  queryParams.query = form.elements.query.value.trim();
   showLoader();
-  
+ 
   if (!queryParams.query) {
     createMessage("The search field can't be empty! Please, enter your request!");
     return;
     hideLoader();
   }
-
+ 
   function createMessage(message) {
     iziToast.show({
       class: 'error-svg',
@@ -52,81 +53,83 @@ async function handleSearch(event) {
   }
 
   try {
-    const {hits, totalHits } = await getImages(queryParams);
+    const { hits, totalHits } = await getImages(queryParams);
     queryParams.maxPage = Math.ceil(totalHits / queryParams.per_page);
-    createMarkup(hits, refs.gallery);  
-    // refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-    gallery.refresh();
+    
+    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(hits));
+    simplyGallery.refresh();
+    hideLoader();
 
-    if (hits.length > 0 && hits.length !== totalHits) {
-      buttonService.show(refs.btnMore); // показуємо кнопку, якщо є результати від серверу і якщо ми не показали всі резульати які є (наприклад, якщо тотал резалст = 100 і кількість артіклів = 100)
-      refs.btnMore.addEventListener("click", handleLoadMore);
+    if (hits.length === 0) {
+      createMessage(`Sorry, there are no images matching your search query. Please, try again!`);
+      hideLoader();
+      
+      } else if (hits.length > 0 && hits.length !== totalHits) {
+      showButton();
+      refs.button.addEventListener("click", handleLoadMore);
       hideLoader();
       
     } else {
-      buttonService.hide(refs.btnMore);
+      hideButton();
       createMessage("We are sorry, but you've reached the end of search results");
     }
 
   } catch (error) {
-    // createMessage("Sorry, there is a problem with connection with the server");
-    console.log(error)
-  
+    createMessage("Sorry, there is a problem with connection with the server");
+      
   } finally {
     hideLoader();
-    refs.form.reset();
+    form.reset();
     scrollBy();
   }
+}
 
-  // обробка натискання на кнопку завантажити більше
   async function handleLoadMore() {
     queryParams.page += 1;
     showLoader();
-   
-    // перед початком запиту - показуємо лоадер і блокуємо кнопку
-  buttonService.disable(refs.btnMore);
+    refs.button.disabled = true;
+        
     try {
       const { hits } = await getImages(queryParams);
-      createMarkup(hits, refs.gallery);//малюємо розмітку
-      
-      // refs.gallery.refresh();
-
-      // if (data.hits.length > 0) {
-      //   showButton();
-      //   refs.btnMore.addEventListener('click', handleLoadMore);
-      // } else {
-      //   hideButton();
-      //   createMessage("We're sorry, but you've reached the end of search results");
-      // }
+      refs.gallery.insertAdjacentHTML("beforeend", createMarkup(hits));
+      simplyGallery.refresh();
 
     } catch (error) {
-      console.log(error);
-      // createMessage("Sorry, there is a problem with connection with the server");
+      createMessage("Sorry, there is a problem with connection with the server");
     
     } finally {
-      hideLoader();     // після запиту - ховаємо лоадер і розблоковуємо кнопку
-    buttonService.enable(refs.btnMore);
-     
-      // і обовʼязково після натискання на кнопку та закінчення запиту перевіряємо, якщо ми зараз знаходимось на останній сторінці - то ховаємо кнопку і видаляємо обробник подій!
-    if (queryParams.page === queryParams.maxPage) {
-      buttonService.hide(refs.btnMore);
-      refs.btnMore.removeEventListener("click", handleLoadMore);
-      createMessage("We are sorry, but you've reached the end of search results");
-    }
-             
+      hideLoader();
+      refs.button.disabled = false;
+           
+      if (queryParams.page === queryParams.maxPage) {
+        hideLoader();
+        hideButton();
+        refs.button.removeEventListener("click", handleLoadMore);
+        createMessage("We are sorry, but you've reached the end of search results");
+      }
       scrollBy();
     }
-  }
-
-  function hideLoader() {
-    setTimeout(() => {
-        refs.loader.classList.add('is-hidden');
-        }, 500);
-  };
-
-  function showLoader() {
-        refs.loader.classList.remove('is-hidden');
-  };
-  
-  
 }
+  
+function hideLoader() {
+      setTimeout(() => {
+        refs.loader.classList.add('is-hidden');
+      }, 500);
+    };
+
+function showLoader() {
+      refs.loader.classList.remove('is-hidden');
+    };
+  
+function hideButton() {
+      refs.button.classList.add('is-hidden');
+    }
+
+function showButton() {
+      refs.button.classList.remove('is-hidden');
+    }
+
+ 
+  
+  
+
